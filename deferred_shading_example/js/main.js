def94f832ae4eed8dbc4a53a8c2bba6db754955c
8eodorosk <8eodorosk@gmail.com>
2019-01-16 13:28:50 +0200

    lala1

--- deferred_shading_example/js/main.js
+++ deferred_shading_example/js/main.js
@@ -58,7 +58,191 @@ const returnProgram = () =>
 
 
 returnProgram().then(programs => {
-	console.log(programs);
+	// console.log(programs[0]);
+
+
+	const geoProgram = programs[0];
+	const mainProgram = programs[1];
+
+	//////////////////////////////////////////
+    // GET GBUFFFER PROGRAM UNIFORM LOCATIONS
+    //////////////////////////////////////////
+    var matrixUniformLocation = gl.getUniformBlockIndex(geoProgram, "Matrices");
+    gl.uniformBlockBinding(geoProgram, matrixUniformLocation, 0);
+    ////////////////////////////
+    // GBUFFER SETUP
+    ////////////////////////////
+    var gBuffer = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, gBuffer);
+
+    gl.activeTexture(gl.TEXTURE0);
+
+    var positionTarget = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, positionTarget);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RGBA16F, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, positionTarget, 0);
+
+    var normalTarget = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, normalTarget);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RGBA16F, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT1, gl.TEXTURE_2D, normalTarget, 0);
+
+    var uvTarget = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, uvTarget);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RG16F, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT2, gl.TEXTURE_2D, uvTarget, 0);
+
+    var depthTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, depthTexture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texStorage2D(gl.TEXTURE_2D, 1, gl.DEPTH_COMPONENT16, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.TEXTURE_2D, depthTexture, 0);
+
+    gl.drawBuffers([
+        gl.COLOR_ATTACHMENT0,
+        gl.COLOR_ATTACHMENT1,
+        gl.COLOR_ATTACHMENT2
+    ]);
+
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+
+    //////////////////////////////////////////////
+    // GET MAIN PROGRAM UNIFORM LOCATIONS
+    //////////////////////////////////////////////
+    var lightUniformsLocation = gl.getUniformBlockIndex(mainProgram, "LightUniforms");
+    gl.uniformBlockBinding(mainProgram, lightUniformsLocation, 0);
+    var eyePositionLocation = gl.getUniformLocation(mainProgram, "uEyePosition");
+    var positionBufferLocation = gl.getUniformLocation(mainProgram, "uPositionBuffer");
+    var normalBufferLocation = gl.getUniformLocation(mainProgram, "uNormalBuffer");
+    var uVBufferLocation = gl.getUniformLocation(mainProgram, "uUVBuffer");
+    var textureMapLocation = gl.getUniformLocation(mainProgram, "uTextureMap");
+    ///////////////////////
+    // GEOMETRY SET UP
+    ///////////////////////
+    var cubeVertexArray = gl.createVertexArray();
+    gl.bindVertexArray(cubeVertexArray);
+    var box = utils.createBox();
+    
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, box.positions, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(0);
+
+    var normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, box.normals, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(1);
+
+    var uvBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, box.uvs, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(2);
+
+    var sphereVertexArray = gl.createVertexArray();
+    gl.bindVertexArray(sphereVertexArray);
+
+    var numCubeVertices = box.positions.length / 3;
+
+    var sphere = utils.createSphere();
+
+    positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, sphere.positions, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(0);
+
+    
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, sphere.indices, gl.STATIC_DRAW);
+    var numSphereElements = sphere.indices.length;
+    gl.bindVertexArray(null);
+    
+    ////////////////////
+    // UNIFORM DATA
+    ////////////////////
+    var projMatrix = mat4.create();
+    mat4.perspective(projMatrix, Math.PI / 2, canvas.width / canvas.height, 0.1, 10.0);
+    var viewMatrix = mat4.create();
+    var eyePosition = vec3.fromValues(1, 1, 1);
+    mat4.lookAt(viewMatrix, eyePosition, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
+    var viewProjMatrix = mat4.create();
+    mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
+      
+    var boxes = [
+        {
+            scale: [1, 1, 1],
+            rotate: [0, 0, 0],
+            translate: [0, 0, 0],
+            modelMatrix: mat4.create(),
+            mvpMatrix: mat4.create(),
+        },
+        {
+            scale: [0.1, 0.1, 0.1],
+            rotate: [0, 0, Math.PI / 3],
+            translate: [0.8, 0.8, 0.4],
+            modelMatrix: mat4.create(),
+            mvpMatrix: mat4.create(),
+        }
+    ];
+
+    var matrixUniformData = new Float32Array(32);
+    var matrixUniformBuffer = gl.createBuffer();
+    gl.bindBufferBase(gl.UNIFORM_BUFFER, 0, matrixUniformBuffer);
+    gl.bufferData(gl.UNIFORM_BUFFER, 128, gl.DYNAMIC_DRAW);
+
+    var lights = [
+        {
+            position: vec3.fromValues(0, 1, 0.5),
+            color:    vec3.fromValues(0.8, 0.0, 0.0),
+            uniformData: new Float32Array(24),
+            uniformBuffer: gl.createBuffer()
+        },
+        {
+            position: vec3.fromValues(1, 1, 0.5),
+            color:    vec3.fromValues(0.0, 0.0, 0.8),
+            uniformData: new Float32Array(24),
+            uniformBuffer: gl.createBuffer()
+        },
+        {
+            position: vec3.fromValues(1, 0, 0.5),
+            color:    vec3.fromValues(0.0, 0.8, 0.0),
+            uniformData: new Float32Array(24),
+            uniformBuffer: gl.createBuffer()
+        },
+        {
+            position: vec3.fromValues(0.5, 0, 1),
+            color:    vec3.fromValues(0.0, 0.8, 0.8),
+            uniformData: new Float32Array(24),
+            uniformBuffer: gl.createBuffer()
+        }
+    ];
+
+
 })
 
 
